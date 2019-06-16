788285fd68a476dbd5671ef5762b4a74e3f7070f
awalone <awalone995@mail.ru>
2019-06-16 23:12:50 +0300

    added a sketch project as a base

---
+++ KKQ2039/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace ShKKQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kolbasabutton = new System.Windows.Forms.Button();
+            this.klabel = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.moneylab = new System.Windows.Forms.Label();
+            this.meatlab = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buymeat = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // kolbasabutton
+            // 
+            this.kolbasabutton.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.kolbasabutton.Location = new System.Drawing.Point(54, 69);
+            this.kolbasabutton.Name = "kolbasabutton";
+            this.kolbasabutton.Size = new System.Drawing.Size(75, 23);
+            this.kolbasabutton.TabIndex = 0;
+            this.kolbasabutton.Text = "колбаса";
+            this.kolbasabutton.UseVisualStyleBackColor = true;
+            this.kolbasabutton.Click += new System.EventHandler(this.kolbasabutton_Click_1);
+            // 
+            // klabel
+            // 
+            this.klabel.AutoSize = true;
+            this.klabel.Location = new System.Drawing.Point(86, 53);
+            this.klabel.Name = "klabel";
+            this.klabel.Size = new System.Drawing.Size(13, 13);
+            this.klabel.TabIndex = 1;
+            this.klabel.Text = "0";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(135, 75);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(50, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "соль";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(54, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "продать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, -1);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(185, 12);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // moneylab
+            // 
+            this.moneylab.AutoSize = true;
+            this.moneylab.Location = new System.Drawing.Point(86, 40);
+            this.moneylab.Name = "moneylab";
+            this.moneylab.Size = new System.Drawing.Size(13, 13);
+            this.moneylab.TabIndex = 7;
+            this.moneylab.Text = "0";
+            this.moneylab.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // meatlab
+            // 
+            this.meatlab.AutoSize = true;
+            this.meatlab.Location = new System.Drawing.Point(86, 27);
+            this.meatlab.Name = "meatlab";
+            this.meatlab.Size = new System.Drawing.Size(13, 13);
+            this.meatlab.TabIndex = 8;
+            this.meatlab.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "мясо";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "чрпш";
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "колб";
+            // 
+            // buymeat
+            // 
+            this.buymeat.Location = new System.Drawing.Point(0, 24);
+            this.buymeat.Name = "buymeat";
+            this.buymeat.Size = new System.Drawing.Size(47, 20);
+            this.buymeat.TabIndex = 12;
+            this.buymeat.Text = "  мясо";
+            this.buymeat.TextChanged += new System.EventHandler(this.buymeat_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(12, 40);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(22, 20);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "+";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(54, -1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "магазин";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 128);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(73, 20);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.Text = "сколько прода";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(184, 162);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.buymeat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.meatlab);
+            this.Controls.Add(this.moneylab);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.klabel);
+            this.Controls.Add(this.kolbasabutton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.Button kolbasabutton;
+        private System.Windows.Forms.Label klabel;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label moneylab;
+        private System.Windows.Forms.Label meatlab;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox buymeat;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ KKQ2039/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShKKQ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {  InitializeComponent();
+          
+        }
+
+      
+        static int kolbasa = 0;
+        static int money = 0;
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void kolbasabutton_Click_1(object sender, EventArgs e)
+        {
+            kolbasa++;
+          
+           
+            initkolb();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+        
+        public void initkolb()
+        {
+            if(kolbasa < 0)
+            {
+                kolbasa = 0;
+            }
+            if (money < 0)
+            {
+                money = 0;
+            }
+            string kolbasacounter = kolbasa.ToString();
+            string moneycounter = money.ToString();
+            klabel.Text = (kolbasacounter);
+
+
+            moneylab.Text = (moneycounter);
+
+            progressBar1.Value = kolbasa;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int sellcount = 0;
+            int.TryParse(textBox1.Text, out sellcount);
+            if (sellcount <= kolbasa)
+            {
+                kolbasa -= sellcount;
+                money += sellcount;
+            }  
+            initkolb();
+         
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            shop a = new shop();
+            a.Show();
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buymeat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
